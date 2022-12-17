c9ed911763c1b3727a4391bb88e5dfdbe359b033
Wong Hoi Sing Edison <hswong3i@pantarei-design.com>
2022-12-17 11:44:09 +0800

    alvistack/0.16.5
        git clean -xdf
        tar zcvf ../python-scikit-build_0.16.5.orig.tar.gz --exclude=.git .
        debuild -uc -us
        cp python-scikit-build.spec ../python-scikit-build_0.16.5-1.spec
        mv ../python*-scikit-build*0.16.5*.{gz,xz,spec,dsc} /osc/home\:alvistack/scikit-build-scikit-build-0.16.5/
        rm -rf ../python*-scikit-build*0.16.5*.* ../python*-skbuild*0.16.5*.*
    
    Signed-off-by: Wong Hoi Sing Edison <hswong3i@pantarei-design.com>

--- setup.py
+++ setup.py
@@ -2,9 +2,6 @@
 
 import setuptools
 
-# Produce an ImportError if this is missing
-import setuptools_scm  # noqa: F401
-
 with open("README.rst") as fp:
     readme = fp.read()
 
@@ -43,6 +40,7 @@ extras_require = {
 
 setuptools.setup(
     name="scikit-build",
+    version="0.16.5",
     description="Improved build system generator for Python C/C++/Fortran/Cython extensions",
     long_description_content_type="text/x-rst; charset=UTF-8",
     long_description=readme + "\n\n" + history,
